e4b2780144caee2404a7a4658afded4ccf4ce4e3
ElBookyes <dtuparov94@gmail.com>
2024-06-05 23:38:43 +0300

    Update: Card component

--- collections/Card/Card.tsx
+++ collections/Card/Card.tsx
@@ -5,14 +5,24 @@ import { CardContainer, CardTitle, CardParagraph } from "./elements";
 
 import { CardProps } from "../../types/CardProps";
 
-export const Card = ({ title = "Default Title", icon="/default-icon.png", href="#", description="No description available" }: CardProps) => {
+export const Card = ({
+  title = "Default Title",
+  icon = "/default-icon.png",
+  href = "#",
+  description = "No description available",
+}: CardProps) => {
   return (
-    <CardContainer aria-label ={`Link to ${title}`} href={href} rel="external" target="_blank">
-      <Image src={icon} alt={`${title} icon`} width={50} height={50}/>
+    <CardContainer
+      aria-label={`Link to ${title}`}
+      href={href}
+      rel="external"
+      target="_blank"
+    >
+      <Image src={icon} alt={`${title} icon`} width={50} height={50} />
       <article>
         <CardTitle>{title}</CardTitle>
         <CardParagraph>
-          <span dangerouslySetInnerHTML={{__html: description}} />
+          <span dangerouslySetInnerHTML={{ __html: description }} />
         </CardParagraph>
       </article>
     </CardContainer>
